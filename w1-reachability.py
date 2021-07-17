759716d28c3d9ce45c04a710eb02e16b6245d7bf
chrisxesq <chrisesq@protonmail.com>
2021-07-17 18:05:21 -0400

    submitted ver.

--- w1-reachability.py
+++ w1-reachability.py
@@ -6,47 +6,41 @@ import sys
 def reach(adj, x, y):
   connected=[]
   def util(adj, x, y):
-      for n in range(len(adj)):
-          if adj[n][0]==x:
-              next = adj[n][1]
-              connected.append(next)
-              #print(adj[n])
-              adj2 = adj[:]
-              adj2[n]='v'
-              if next == y:
-                ans = 1
-                return ans
-              util(adj2, next, y)
-        
-      return 0
-  util(adj, x, y)
-  if y in connected:
-    return 1
+      if len(adj[x])==0:
+        return connected
+      else:
+        for i in range(len(adj[x])):
+          new = adj[x][i]
+          if new == 'visited':
+              return connected
+          if new not in connected:
+            connected.append(adj[x][i])
+          adj[x][i]='visited'
+          if new==y:
+            return connected
+          else:
+            util(adj, new, y)
+        return connected
+  list = util(adj,x,y)
+  if x in list and y in list:
+      return 1
   else:
-    return 0
+      return 0
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     data = list(map(int, input.split()))
-    #print('data',data)
     n, m = data[0:2]
-    #print('n,m', n, m)
     data = data[2:]
-    edges = list(zip[data[0:(2 * m):2], data[1:(2 * m):2]])
-    #print('data,edges', data, edges)
+    edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
     x, y = data[2 * m:]
-    #print('x,y',x,y)
     adj = [[] for _ in range(n)]
-    #print('adj',adj)
     x, y = x - 1, y - 1
-    #print('x,y',x,y)
     for (a, b) in edges:
-        print('a,b',a,b)
         adj[a - 1].append(b - 1)
         adj[b - 1].append(a - 1)
-        # print('adj',adj)
-    
     print(reach(adj, x, y))
+
     
 # # Input:
 # 4 3
